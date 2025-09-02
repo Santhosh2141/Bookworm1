6044a4143b75c4e724df7a541390570599dee101
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-02 20:47:21 +0530

    Initial Commit

---
+++ Bookworm1/Bookworm1App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm1App.swift
+//  Bookworm1
+//
+//  Created by Santhosh Srinivas on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Bookworm1
+//
+//  Created by Santhosh Srinivas on 02/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
