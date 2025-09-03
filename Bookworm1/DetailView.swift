9466a80d44898c6fe4908680721421de890fc0d3
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-04 00:48:33 +0530

    delete any books

--- Bookworm1/DetailView.swift
+++ Bookworm1/DetailView.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct DetailView: View {
-    
     let book: Book
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showDeleteAlert = false
+//    var completedDate = ""
     var body: some View {
         ScrollView{
             ZStack(alignment: .bottomTrailing){
@@ -24,12 +27,21 @@ struct DetailView: View {
                     .background(.black)
                     .opacity(0.5)
                     .clipShape(Capsule())
-                    .offset(x: 5,y: 5)
-                
+                    .offset(x: -5,y: -5)
             }
             Text(book.author ?? "Unkown Author")
                 .font(.title)
                 .foregroundColor(.secondary)
+            // doing this because its and optional
+            if let date = book.dateAdded {
+                Text("Date Read: \(date.formatted(date: .abbreviated, time: .shortened))")
+                    .padding()
+                    .font(.caption)
+            } else {
+                Text("Date Read: Unknown")
+                    .padding()
+                    .font(.caption)
+            }
             Text(book.review ?? "No Review")
                 .padding()
             RatingView(rating: .constant(Int(book.rating)))
@@ -37,6 +49,25 @@ struct DetailView: View {
         }
         .navigationTitle(book.title ?? "Unkown Title")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete the Book?", isPresented: $showDeleteAlert){
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel){ }
+        } message: {
+            Text("Are you sure you want to delete this book")
+        }
+        .toolbar{
+            Button{
+                showDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+    }
+    
+    func deleteBook(){
+        moc.delete(book)
+        try? moc.save()
+        dismiss()
     }
 }
 
